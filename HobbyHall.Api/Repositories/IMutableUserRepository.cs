8f69937f50ca19feea2e803ebc76d084e3f2901f
danny alcantara <danny.alcantara@bestbuy.com>
2020-08-26 06:58:25 -0400

    Adding missing refactoring to seggregate IRepository interfaces

---
+++ HobbyHall.Api/Repositories/IMutableUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HobbyHall.Api.Models;
+
+namespace HobbyHall.Api.Repositories
+{
+    public interface IMutableUserRepository
+    {
+        Task<User> Create(User user);
+        Task<User> Update(User user);
+        void Delete(int id);
+    }
+}
